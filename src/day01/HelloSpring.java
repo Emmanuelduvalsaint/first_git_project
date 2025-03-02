5db38f9e23a24cf751751c96bd3e7a57814ac51b
Emmanuelduvalsaint <emduvalsaint@gmail.com>
2025-03-02 10:27:22 -0500

    HelloSpring Updated

--- src/day01/HelloSpring.java
+++ src/day01/HelloSpring.java
@@ -3,5 +3,8 @@ package day01;
 public class HelloSpring {
     public static void main(String[] args) {
         System.out.println("Hello Spring");
+        String spring = "Spring is beautiful";
+
+        System.out.println(spring);
     }
 }
