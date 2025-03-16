97b3ca8598aebfe778b9291e07ea1d32ffd3d42b
Emmanuelduvalsaint <124452372+Emmanuelduvalsaint@users.noreply.github.com>
2025-03-16 09:45:05 -0400

    Update HelloWorld.java

--- src/day01/HelloWorld.java
+++ src/day01/HelloWorld.java
@@ -5,5 +5,6 @@ public class HelloWorld {
         System.out.println("Hello World");
 
         System.out.println("Hello World, it is nice");
+        System.out.println("Hello World, it is blue sea");
     }
 }
